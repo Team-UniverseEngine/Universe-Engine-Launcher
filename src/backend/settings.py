624c9e80563e9ad12a842210a0114fb7a5becfc3
Daveberry <84567826+daveberrys@users.noreply.github.com>
2025-10-05 12:26:19 +0800

    leaving this one to videobot or char to continue
    ion got no motivation to work on this shi

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -1,18 +1,20 @@
 # code taken from noteted!!!
 # foss app by daveberry blueson
 
-
 import os
 import sys
 import json
 
+import customtkinter as ctk
+import tkinter as tk
+
 def getAppConfigDirectory():
     if sys.platform == 'win32':
-        return os.path.join(os.getenv('APPDATA'), 'Noteted')
+        return os.path.join(os.getenv('APPDATA'), 'Universe Engine Launcher') # type: ignore
     elif sys.platform == 'linux': # Linux
-        return os.path.join(os.getenv('XDG_CONFIG_HOME', os.path.join(os.path.expanduser('~'), '.config')), 'Noteted')
+        return os.path.join(os.getenv('XDG_CONFIG_HOME', os.path.join(os.path.expanduser('~'), '.config')), 'Universe Engine Launcher')
     elif sys.platform == 'darwin': # macOS
-        return os.path.join(os.path.expanduser('~'), 'Library', 'Application Support', 'Noteted')
+        return os.path.join(os.path.expanduser('~'), 'Library', 'Application Support', 'Universe Engine Launcher')
     # fallback for other systems or if APPDATA is not set
     return os.path.join(os.path.expanduser('~'), '.noteted')
 
@@ -33,3 +35,33 @@ def saveSettings(settingsData):
     print(f"Attempting to save settings to: {settingsFile}")
     with open(settingsFile, 'w') as f:
         json.dump(settingsData, f, indent=4)
+        
+def getSettingsDef():
+    settingsDef = [
+        {
+            "name": "Automatically check for updates?",
+            "type": "bool",
+            "default": True,
+            "id": "automaticUpdate"
+        }
+    ]
+    
+    return settingsDef
+
+def listAllSettings(parent):
+    settingsDef = getSettingsDef()
+    for settings in settingsDef:
+        settingName = settings["name"]
+        settingType = settings["type"]
+        settingDefault = settings["default"]
+        settingID = settings["id"]
+        
+        settingFrame = ctk.CTkFrame(parent, fg_color="transparent")
+        settingFrame.pack(pady=5, padx=10, fill="x")
+
+        settingLabel = ctk.CTkLabel(settingFrame, text=settingName)
+        settingLabel.pack(side="left", padx=(0, 5))
+        
+        if settingType == "bool":
+            settingCheckbox = ctk.CTkCheckBox(settingFrame)
+            settingCheckbox.pack(side="right", padx=10)
