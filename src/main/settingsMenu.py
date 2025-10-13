e6a82e1df681d5f659c5a7024503f230fb19d321
VideoBot <lander.meersman@gmail.com>
2025-10-13 15:53:01 +0200

    settings saving

--- src/main/settingsMenu.py
+++ src/main/settingsMenu.py
@@ -2,10 +2,13 @@ import customtkinter as ctk
 import src.handler.topLevelIcon as TLI
 import src.backend.settings as settings
 
+curSettings = settings.loadSettings()
+
 def init():
     settingsWindow = ctk.CTkToplevel()
     settingsWindow.title("Settings")
     settingsWindow.geometry("360x240")
+    settingsWindow.resizable(False, True)
     
     settingsWindow.transient()
     settingsWindow.after(10, settingsWindow.grab_set)
@@ -15,29 +18,8 @@ def init():
     saveUI(settingsWindow)
 
 def ui(parent):
-    mainFrame = ctk.CTkFrame(parent, fg_color="transparent")
-    mainFrame.pack(pady=10, padx=10)
-
-    settingsButtons = [
-        {
-            "buttonName": "TestButton",
-            "execCommand": parent.destroy,
-            "padY": (5, 10),
-            "side": "top"
-        }
-    ]
-
-    buttonFrame = ctk.CTkFrame(mainFrame, fg_color="#1E1E1E")
-    buttonFrame.pack(side="bottom", fill="y", expand=True)
-    
-    for buttons in settingsButtons:
-        nameButton = buttons["buttonName"]
-        commandExecute = buttons["execCommand"]
-        paddingX = buttons["padY"]
-        buttonSide = buttons["side"]
-    
-        itemButton = ctk.CTkButton(buttonFrame, fg_color="#1B1B1B", text=nameButton, command=commandExecute)
-        itemButton.pack(padx=paddingX, pady=10, fill="y", expand=True, side=buttonSide)
+    settings.loadSettings()
+    settings.listAllSettings(parent)
 
 
 def saveUI(parent):
@@ -47,7 +29,7 @@ def saveUI(parent):
     settingsButtons = [
         {
             "buttonName": "save",
-            "execCommand": parent.destroy,
+            "execCommand": save,
             "padX": (5, 10),
             "side": "left"
         },
@@ -70,3 +52,7 @@ def saveUI(parent):
     
         itemButton = ctk.CTkButton(buttonFrame, fg_color="#1B1B1B", text=nameButton, command=commandExecute)
         itemButton.pack(padx=paddingX, pady=10, fill="x", expand=True, side=buttonSide)
+
+def save():
+    settings.saveSettings(curSettings)
+    
